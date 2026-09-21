3f86219a3109d3f979a0538abc022ed30eaffd18
오동운 <kku0022@gmail.com>
2026-09-21 15:23:12 +0900

    solving Conflict

--- practice/week3/age.cpp
+++ practice/week3/age.cpp
@@ -4,11 +4,7 @@ using namespace std;
 int main() 
 {
     int age; // 나이
-<<<<<<< HEAD
     cout << "나이를 입력해주세요.";
-=======
-    cout << "나이를 입력해";
->>>>>>> dce9cae7fabcf407006f86741c7a1e40978d8822
     cin >> age;
 
     if (age <= 12)
